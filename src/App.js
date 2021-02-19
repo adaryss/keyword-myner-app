6082ba7281b882c1383466aebf7d8c97ac02ef25
Adam Rysanek <rysanek.adam@gmail.com>
2021-02-20 00:08:03 +0100

    Add material ui and dummy content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Typography } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Typography vyriant="h1" color="secondary">
+                    Keyword Myner ⛏️
+                </Typography>
+            </header>
+        </div>
+    );
+};
 
 export default App;
